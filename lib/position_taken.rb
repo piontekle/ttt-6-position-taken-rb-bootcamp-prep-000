92a173f66ca620ac562c08c63677fff1b5f63e8d
Lauren Piontek <piontekle@gmail.com>
2018-10-22 18:24:45 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,7 @@
 # code your #position_taken? method here!
 
-def position_taken?(board, index)
-  
+def position_taken?(board, index_number)
+  if array[index_number] == " "
+    return false
+  end
 end
