cf36aba5a57335c7cbc2ce23dca0ff4b87016545
Lauren Piontek <piontekle@gmail.com>
2018-10-22 18:29:15 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,5 +3,7 @@
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
+    elsif board[index] == X || O
+    return true
   end
 end
